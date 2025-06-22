cf20bab8ab01b744d199b04b16170bb3cb11db02
ohjohwan <whghks1020@naver.com>
2025-06-22 10:00:36 +0900

    배포를 위한 훅 코드 숨김 처리

--- src/hooks/useCowPregnancy.tsx
+++ src/hooks/useCowPregnancy.tsx
@@ -1,9 +1,9 @@
-const cowPragnency = (aiDate) => {
-  let dueDate; // 분만 예정일
-  let nextEstrus; // 재발정일
+// const cowPragnency = (aiDate) => {
+//   let dueDate; // 분만 예정일
+//   let nextEstrus; // 재발정일
 
-  dueDate = aiDate + 20;
-  nextEstrus = aiDate - 90 + 10;
-};
+//   dueDate = aiDate + 20;
+//   nextEstrus = aiDate - 90 + 10;
+// };
 
-export default cowPragnency;
+// export default cowPragnency;
